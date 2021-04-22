f31cba7125dec0b742b3c34dec667943cb11c4f6
Davinderpro01 <47469466+Davinderpro01@users.noreply.github.com>
2021-04-22 15:01:59 -0600

    Add files via upload

---
+++ ProductosAPI/target/classes/import.sql
@@ -0,0 +1,3 @@
+insert into produtos(id,nombre,precio) value (1 , 'audifonos' , 100);
+insert into produtos(id,nombre,precio) value (2 , 'camara' , 2500);
+insert into produtos(id,nombre,precio) value (3 , 'smartphone' , 1300);
